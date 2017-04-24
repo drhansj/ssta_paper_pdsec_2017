0e0dc5f8184f70c22e983d56eebcc42dd398cf50
Bryce Adelstein-Lelbach aka wash <balelbach@lbl.gov>
2017-04-24 13:44:51 -0700

    Add font-embedding support to the Makefile and unify the font-embedding technique in the Makefile with embed_pdf_fonts.sh

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (c) 2016 Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
+# Copyright (c) 2016-7 Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -13,6 +13,9 @@ pdf:
 	bibtex $(PAPER)
 	-$(PDFTEX) --interaction=batchmode $(PAPER)
 	$(PDFTEX) $(PAPER)
+	gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -dNOPLATFONTS -sOutputFile=$(PAPER).embed.pdf -f $(PAPER).pdf
+	/bin/rm -f $(PAPER).pdf
+	mv $(PAPER).embed.pdf $(PAPER).pdf
 
 clean:
 	/bin/rm -f *.dvi *.log *.aux *.toc *.bbl *.blg *.out *.upa *.upb *.lox *.tdo
--- figures/embed_pdf_fonts.sh
+++ figures/embed_pdf_fonts.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#! /bin/bash
+###############################################################################
+# Copyright (c) 2016-7 Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+###############################################################################
 
-# gs -dCompatibilityLevel=1.4 \
-#    -dPDFSETTINGS=/screen \
-#    -dCompressFonts=true \
-#    -dSubsetFonts=true \
-#    -dNOPAUSE \
-#    -dBATCH \
-#    -sDEVICE=pdfwrite \
-#    -sOutputFile=${1}.embed.pdf \
-#    -c ".setpdfwrite <</NeverEmbed [ ]>> setdistillerparams" \
-#    -f ${1}.pdf
-
-gs -q -dNOPAUSE -dBATCH -dPDFSETTINGS=/prepress -sDEVICE=pdfwrite -sOutputFile=${1}.embed.pdf ${1}.pdf
+gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dEmbedAllFonts=true -dNOPLATFONTS -sOutputFile=${1}.embed.pdf ${1}.pdf
